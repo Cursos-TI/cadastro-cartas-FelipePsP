44bf4518698fc5193e4e9f78b953e1f54a167812
FelipePsP <felipepiresdepaula@hotmail.com>
2025-12-23 10:22:50 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 
 int main(){
-  
-    // Área para definição das variáveis para armazenar as propriedades das cidades
-    int Estado;
+    
+    char Estado[50];
     char CódigodaCarta[50];
     char NomedaCidade[50];
     int População;
     float Área;
     float PIB;
     int NúmerodePontosTurísticos;
-    
-    // Área para entrada de dados
+    float DensidadePopulacional;
+    float PIBpercapita;
+
+
+
+  //COLETA DE DADOS
     printf("Digite o estado: ");
-    scanf("%i", &Estado);
+    scanf("%s", Estado);
 
     printf("Digite o Código da carta: ");
     scanf("%s", &CódigodaCarta);
 
     printf("Digite o Nome da cidade: ");
-    scanf("%s", &NomedaCidade);
-
+    scanf(" %[^\n]", NomedaCidade);
+    
     printf("Digite a população: ");
     scanf("%i", &População);
 
@@ -33,17 +36,25 @@ int main(){
     printf("Digite o número de pontos turisticos: ");
     scanf("%i", &NúmerodePontosTurísticos); 
 
-    // Área para exibição dos dados da cidade
-    printf("DADOS DA CARTA\n");
-    printf("Estado: %i\n", Estado);
-    printf("Código da carta: %s\n", CódigodaCarta);
+  //CALCULO DA DENSIDADE
+    DensidadePopulacional = População / Área;
+
+  //CALCULO DO PIB Per Capita
+    PIBpercapita = (PIB * 1000000000.0f) / População;
+   
+
+  //Amostra dos dados
+    printf("***DADOS DA CARTA***\n");
+    printf("Estado: %s\n", Estado);
+    printf("Código da carta: %i\n", CódigodaCarta);
     printf("Nome da cidade: %s\n", NomedaCidade);
     printf("População: %i\n", População);
     printf("Área: %.2f Km²\n", Área);
     printf("PIB: %.2f bilhões de reais\n", PIB);
     printf("Número de pontos turisticos: %i\n", NúmerodePontosTurísticos);
+    printf("Densidade populacional: %.2f hab/km²\n", DensidadePopulacional);
+    printf("PIB per Capita: %.2f reais\n", PIBpercapita);
+   
+
    
     return 0;
-
-
-}
